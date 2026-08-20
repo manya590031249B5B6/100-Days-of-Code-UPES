c47b9bef96bb9ab6fcf3de52bb20d3f0aab036f1
Manya Aroragit config --global user.email manya.31249@stu.upes.ac.in <manya.31249@stu.upes.ac.in>
2026-08-20 18:21:22 +0530

    Day 1 -Q1 solution

---
+++ DAY1_Q1.c
@@ -0,0 +1,11 @@
+// Write a program to input of two numbers and display their sum.
+#include<stdio.h>
+
+int main() {
+int n1,n2,sum;
+printf("\nEnter any two no.=");
+scanf("%d %d",&n1,&n2);
+sum=n1+n2;
+printf("\nSum of two no. is %d",sum);
+return 0;
+}
