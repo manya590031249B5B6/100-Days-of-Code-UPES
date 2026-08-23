0ac37459b402fdf7312596864acb3285ffea057e
Manya Aroragit config --global user.email manya.31249@stu.upes.ac.in <manya.31249@stu.upes.ac.in>
2026-08-24 00:20:48 +0530

    day6_q1 completed

---
+++ day6_q1.c
@@ -0,0 +1,28 @@
+/*
+Q11: Write a program to input an integer and check whether it is even or odd using if–else.
+
+/*
+Sample Test Cases:
+Input 1:
+7
+Output 1:
+7 is odd
+
+Input 2:
+12
+Output 2:
+12 is even
+
+*/
+#include<stdio.h>
+
+int main() {
+int num;
+printf("\nEnter any no.=");
+scanf("%d",&num);
+if(num%2==0)
+printf("\nGiven no. is an even no.");
+else
+printf("\nGiven no. is an odd no.");
+return 0;
+}
