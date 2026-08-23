a2fed42865f39afa7bc0dcd19ac34d40e9a96ba4
Manya Aroragit config --global user.email manya.31249@stu.upes.ac.in <manya.31249@stu.upes.ac.in>
2026-08-24 01:25:25 +0530

    day7 q2 completed

---
+++ day 7_q2.c
@@ -0,0 +1,28 @@
+/*
+Q14: Write a program to input a character and check whether it is a vowel or consonant using if–else.
+
+/*
+Sample Test Cases:
+Input 1:
+a
+Output 1:
+Vowel
+
+Input 2:
+b
+Output 2:
+Consonant
+
+*/
+#include<stdio.h>
+
+int main() {
+char vowels,consonants,character;
+printf("\nEnter any character value=");
+scanf("%c",&character);
+if(character=='a' || character=='e' || character=='i' || character=='o' || character=='u')
+printf("\nGiven character is vowel");
+else
+printf("\nGiven character is consonant");
+return 0;
+} 
