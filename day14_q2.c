f6a0eb30a1ff244bfbec44b0f6054641f8562bd5
Manya Aroragit config --global user.email manya.31249@stu.upes.ac.in <manya.31249@stu.upes.ac.in>
2026-08-28 18:49:45 +0530

    day15 q1 completed

---
+++ day14_q2.c
@@ -0,0 +1,31 @@
+/*
+Q28: Write a program to print the product of even numbers from 1 to n.
+
+/*
+Sample Test Cases:
+Input 1:
+4
+Output 1:
+8 (2 * 4)
+
+Input 2:
+6
+Output 2:
+48 (2 * 4 * 6)
+
+*/
+#include<stdio.h>
+
+int main() {
+int n,count=0,i,product=0;
+printf("\nEnter any number=");
+scanf("%d",&n);
+i=0;
+for(count=0;count<=n;count++)
+{d
+    product=product*i;
+    i=i+2;
+}
+printf("\nProduct of any no. is %d",product);
+return 0;
+}
