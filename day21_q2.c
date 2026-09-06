dc692202c237b1c2f55e475ce6e7e561361bcade
Manya Aroragit config --global user.email manya.31249@stu.upes.ac.in <manya.31249@stu.upes.ac.in>
2026-09-06 09:36:45 +0530

    day21 q2 completed

---
+++ day21_q2.c
@@ -0,0 +1,39 @@
+/*
+Q42: Write a program to check if a number is a perfect number.
+
+/*
+Sample Test Cases:
+Input 1:
+6
+Output 1:
+Perfect number
+
+Input 2:
+10
+Output 2:
+Not perfect number
+
+*/
+#include<stdio.h>
+
+int main() {
+    int n,i=1,sum=0;
+printf("\nEnter any number=");
+scanf("%d",&n);
+for(i=1;i<n;i++)
+{
+    if(n %i==0)
+    {
+        sum=sum+i;
+    }
+}
+
+if (n==sum)
+printf("\nPerfect number");
+else
+printf("\nNot perfect number");
+
+fflush(stdout);
+
+return 0;
+}
