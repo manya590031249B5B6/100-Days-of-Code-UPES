218384f0e97e85df8bcd690b92f577dc55efaf16
Manya Aroragit config --global user.email manya.31249@stu.upes.ac.in <manya.31249@stu.upes.ac.in>
2026-09-06 20:50:13 +0530

    day25 q2 completed

--- day25-q2.c
+++ day25-q2.c
@@ -23,3 +23,24 @@ Output 2:
 Note: Spaces indicate indentation.
 
 */
+#include<stdio.h>
+
+int main() {
+int r,i,j;
+printf("\nEnter no. of rows=");
+scanf("%d",&r);
+for(i=r;i>=1;i--)
+{
+    
+    for(j=1;j<=r-i;j++)
+    {
+        printf(" ");
+    }
+    for(j=1;j<=i;j++)
+    {
+        printf("*");
+    }
+printf("\n");
+}
+return 0;
+}
