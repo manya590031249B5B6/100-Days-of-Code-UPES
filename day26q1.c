ba05dfebd14aff31e93acb8cb8b2c8f05333c65c
Manya Aroragit config --global user.email manya.31249@stu.upes.ac.in <manya.31249@stu.upes.ac.in>
2026-09-07 07:59:44 +0530

    day26q1 completed

---
+++ day26q1.c
@@ -0,0 +1,43 @@
+/*
+Q51: Write a program to print the following pattern:
+    5
+   45
+  345
+ 2345
+12345
+
+/*
+Sample Test Cases:
+Input 1:
+
+Output 1:
+    5
+   45
+  345
+ 2345
+12345
+
+*/
+#include<stdio.h>
+
+int main() {
+int n,r,i,j;
+printf("\nenter any no. of rows=");
+scanf("%d",&n);
+
+r=1;
+
+for(i=1;i<=n;i++)
+{
+    printf("\n");
+
+for(j=i;j<n;j++)
+{
+    printf(" ");
+}
+r=r*10;
+
+printf("%d",12345%r);
+}
+return 0;
+}
