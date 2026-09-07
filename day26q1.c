a203148161f2784d5fc3387afb576bae44d4130b
Manya Aroragit config --global user.email manya.31249@stu.upes.ac.in <manya.31249@stu.upes.ac.in>
2026-09-07 08:27:37 +0530

    day26q2 completed

--- day26q1.c
+++ day26q1.c
@@ -5,7 +5,7 @@ Q51: Write a program to print the following pattern:
   345
  2345
 12345
-
+                                                                                                                                                                                     
 /*
 Sample Test Cases:
 Input 1:
